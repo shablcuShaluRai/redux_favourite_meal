e14832e4bf3a248a8c936c7fc8af3830a465a789
shalurai <shaluraics22@gmail.com>
2017-08-16 21:24:55 +0900

    create a redux store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux'
+import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer)
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
